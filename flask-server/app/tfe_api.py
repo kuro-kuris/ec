625c096066e9e2cf1d1fdac229d27b9afc5d4bd0
Chris Swart <cswart@outlook.com>
2015-03-11 23:51:12 +0000

    added python wrapper around the tfe api with simple unit tests checking status code of responses, currently I have no idea how to operate directions

---
+++ flask-server/app/tfe_api.py
@@ -0,0 +1,133 @@
+import json
+import requests
+import os
+
+API_KEY = os.environ.get("TFE_API_KEY")
+
+general_url = 'https://tfe-opendata.com/api/v1/'
+
+essential_parameters = dict(
+	key = API_KEY
+)
+
+def getStops():
+
+	url = general_url + 'stops'
+
+	parameters = essential_parameters
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def serviceUpdates():
+
+	url = general_url + 'services'
+
+	parameters = essential_parameters
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def timeTable(stop_id):
+
+	url = general_url + 'timetables/' + str(stop_id)
+
+	parameters = dict(
+		stop_id = stop_id
+	)
+
+	parameters.update(essential_parameters)
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def journeys(service_name):
+
+	url = general_url + '/journeys/' + service_name
+
+	parameters = dict(
+		service_name = unicode(service_name)
+	)
+
+	parameters.update(essential_parameters)
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def stop2stop(start_stop_id, end_stop_id, date, duration):
+
+	url = general_url + '/stoptostop-timetable/'
+
+	parameters = dict(
+		start_stop_id = start_stop_id,
+		finish_stop_id = end_stop_id,
+		date = date,
+		duration = duration
+	)
+
+	parameters.update(essential_parameters)
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def disruptions():
+
+	url = general_url + '/status'
+
+	parameters = essential_parameters
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def directions(initial_location, destination_location, date, time_mode):
+
+	url = general_url + '/directions/'
+
+	parameters = dict(
+		start = initial_location,
+		finish = destination_location,
+		date = date,
+		time_mode = unicode(time_mode)
+	)
+
+	parameters.update(essential_parameters)
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+def live_bus_location():
+
+	url = general_url + 'vehicle_locations'
+
+	parameters = essential_parameters
+
+	response = requests.get(url = url, params = parameters)
+
+	return response
+
+
+
+# # open a file
+
+# functions = 
+
+# f = open("tfe_" + function + ".txt", "w")
+
+# match_history_json = getMatchHistory()
+
+# json.dump(match_history_json, f, sort_keys = True, indent = 4, ensure_ascii = False)
+
+# f.close()
+
+# f = open("game_details.txt", "w")
+
+# game_details_json = getMatchDetails(789645621)
+
+# json.dump(game_details_json, f, sort_keys = True, indent = 4, ensure_ascii = False)
