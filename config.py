fddbd1fd2d32dafd2af6458df476180657662ff3
Chris Swart <cswart@outlook.com>
2015-04-06 20:21:03 +0100

    janitorial work

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
