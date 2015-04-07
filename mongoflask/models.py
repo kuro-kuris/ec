29d657e74da8414eedbe04d31546a8cd0da51fd4
Chris Swart <cswart@outlook.com>
2015-04-07 12:37:55 +0100

    trying to get the models to work

--- mongoflask/models.py
+++ mongoflask/models.py
@@ -2,23 +2,16 @@ from flask import url_for
 from mongoflask import db
 
 class Stop(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
-    stop_id = db.Column(db.Integer, primary_key = True)
+    stop_id = db.Column(db.Integer)
     orientation = db.Column(db.Integer)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
 
     def __repr__(self):
         return self.name
-
-class Route(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    destination_id = db.Column(db.Integer, db.ForeignKey(Stop.stop_id))
-    stops = db.relationship('Stop', backref = 'route')
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
-
-    def __repr__(self):
-        return "Heading to " + self.destination
 
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -27,3 +20,13 @@ class Service(db.Model):
 
     def __repr__(self):
         return self.name
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    stops = db.relationship('Stop', backref = 'route')
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+
+    def __repr__(self):
+        return "Heading to " + self.destination
+
+
