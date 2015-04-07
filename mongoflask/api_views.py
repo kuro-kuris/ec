fc3150a104c25f7794d7fe83e297d62519eb2862
Chris Swart <cswart@outlook.com>
2015-04-07 12:37:40 +0100

    moved to static model that uses the json files from api calls

--- mongoflask/api_views.py
+++ mongoflask/api_views.py
@@ -1,25 +1,34 @@
 from flask import Blueprint
 from flask_restful import Api, Resource, url_for, abort
-from mongoflask.models import Service, Stop
+from mongoflask.models import Service, Route, Stop
+from mongoflask import db
+from mongoflask.brain import *
+import json
+
+
+
+
+# def abort_if_service_doesnt_exist(name):
+#       if len(Service.objects(name = unicode(name))) == 0:
+#           abort(404, message="Service number {} doesn't exist".format(name))
 
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-def abort_if_service_doesnt_exist(name):
-		if len(Service.objects(name = unicode(name))) == 0:
-			abort(404, message="Service number {} doesn't exist".format(name))
+
 
 class BusStops(Resource):
 
 	
     def get(self, name):
-    	abort_if_service_doesnt_exist(name)
-    	service = Service.objects(name = unicode(name)).get()
+    	#abort_if_service_doesnt_exist(name)
+    	routes = getServiceStops(unicode(name)) 
         response = {}
-        for stop in service.stops:
-        	response.update({'Name' : stop.name})
+        response.update({'Route' : routes[0]})
         return response
+
+
 
 # Register resources
 
