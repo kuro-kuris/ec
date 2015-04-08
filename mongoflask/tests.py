f955636b1ef488c1e442c3b07f07b65cac46bd06
Chris Swart <cswart@outlook.com>
2015-04-08 17:47:28 +0100

    added closest stop test method

--- mongoflask/tests.py
+++ mongoflask/tests.py
@@ -25,9 +25,19 @@ class brain_TestCase(unittest.TestCase):
 	def test_getClosestStop(self):
 		stop_list = getServiceStops('44')
 		position = stop_list[2]
-		real_closest_stop = stop_list[2]
-		assumed_closest_stop = getClosestStop()
-		assert True == True
+		real_closest_stop = stop_list[3]
+		assumed_closest_stop = getClosestStop(position['latitude'], position['longitude'], position['orientation'], stop_list)
+		print "Assumed closest stop: "
+		print assumed_closest_stop
+		print "1"
+		print stop_list[1]
+		print "2"
+		print stop_list[2]
+		print "3"
+		print stop_list[3]
+
+
+		assert real_closest_stop == assumed_closest_stop
 
 
 
